2add8887b3a4529cd24ff2754fe18ec09b3aeb98
krizal1 <159939607+krizal1@users.noreply.github.com>
2024-03-27 14:57:55 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,44 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+    T* arr;
+    int top;
+
+public:
+    TStack() {
+        arr = new T[size];
+        top = -1;
+    }
+
+    ~TStack() {
+        delete[] arr;
+    }
+
+    void push(T value) {
+        if (top < size - 1) {
+            arr[++top] = value;
+        }
+    }
+
+    T pop() {
+        if (top >= 0) {
+            return arr[top--];
+        }
+        return T();
+    }
+
+    T get() const {
+        return arr[top];
+    }
+
+    bool isEmpty() const {
+        return top == -1;
+    }
+
+    bool isFull() const {
+        return top == size - 1;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
