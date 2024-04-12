2126d3dd7051bc91a60e1cdee1603125e4c62b16
krizal1 <159939607+krizal1@users.noreply.github.com>
2024-04-12 14:01:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -17,15 +17,15 @@ std::string infx2pstfx(std::string inf) {
     for (char c : inf) {
         if (isdigit(c)) {
             postfix = postfix + c + ' ';
-        }else if (c == '(') {
+        } else if (c == '(') {
             stack.push(c);
-        }else if (isOperator(c)) {
+        } else if (isOperator(c)) {
             while (!stack.isEmpty() && Priority(stack.get()) >= Priority(c)) {
                 postfix = postfix + stack.get() + ' ';
                 stack.pop();
             }
             stack.push(c);
-        }else if (c == ')') {
+        } else if (c == ')') {
             while (!stack.isEmpty() && stack.get() != '(') {
                 postfix = postfix + stack.get() + ' ';
                 stack.pop();
@@ -48,7 +48,7 @@ int eval(std::string post) {
     for (char c : post) {
         if (isdigit(c)) {
             Number += c;
-        }else if (Number != "") {
+        } else if (Number != "") {
             stack.push(std::atoi(Number.c_str()));
             Number = "";
         }
